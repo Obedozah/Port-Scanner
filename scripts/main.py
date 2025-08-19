5a794dc42b705f904db98ef8dec6bc62a981db2c
Avery Lopez <averyagustin05@gmail.com>
2025-08-18 18:24:39 -0700

    Multiple Scan Functionality

--- scripts/main.py
+++ scripts/main.py
@@ -3,4 +3,6 @@ import testSocket
 if __name__ == "__main__":
     while True:
         testSocket.runUserInput()
-        break;
+        anotherScan = input("Would you like to scan more ports (y/n)").lower()
+        if anotherScan != "y" or anotherScan != "yes":
+            break;
